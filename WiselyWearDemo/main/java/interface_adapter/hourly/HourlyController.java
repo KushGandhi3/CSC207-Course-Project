6ce106d5d8793d0cfa02a2e8b0e858153d62be84
chika <chika.peynisa@mail.utoronto.ca>
2024-11-10 03:48:18 -0500

    Finished HourlyController

---
+++ WiselyWearDemo/main/java/interface_adapter/hourly/HourlyController.java
@@ -0,0 +1,25 @@
+package interface_adapter.hourly;
+
+import use_case.hourly.HourlyInputBoundary;
+
+/**
+ * Controller for the hourly Use Case.
+ */
+public class HourlyController {
+    private final HourlyInputBoundary hourlyUseCaseInteractor;
+
+    public HourlyController(HourlyInputBoundary hourlyUseCaseInteractor) {
+        this.hourlyUseCaseInteractor = hourlyUseCaseInteractor
+    }
+
+    /**
+     * Executes the Hourly Use Case.
+     * @param location the location for which to fetch the hourly weather forecast
+     */
+    public void execute(String location) {
+        final HourlyInputData inputData = new HourlyInputData(location);
+
+        // Execute the use case, which fetches the data for hourly weather.
+        hourlyUseCaseInteractor.execute(inputData);
+    }
+}
