c249b24d87c88e0120256abe926ae0074c90cae8
Kush Gandhi <99158764+KushGandhi3@users.noreply.github.com>
2024-11-10 23:14:37 -0500

    Updated DAO

--- WiselyWearDemo/main/java/data_access/WeatherDataAccessObject.java
+++ WiselyWearDemo/main/java/data_access/WeatherDataAccessObject.java
@@ -1,5 +1,6 @@
 package data_access;
 
+import entity.WeatherFactory;
 import use_case.display_home.DisplayHomeWeatherDataAccessInterface;
 
 import java.io.BufferedReader;
@@ -14,6 +15,11 @@ public class WeatherDataAccessObject implements DisplayHomeWeatherDataAccessInte
 
     private static final String API_KEY = ""; // ADD API KEY HERE;
     private static final String API_URL = "https://api.openweathermap.org/data/3.0/onecall";
+    private final WeatherFactory weatherFactory;
+
+    public WeatherDataAccessObject(WeatherFactory weatherFactory) {
+        this.weatherFactory = weatherFactory;
+    }
 
     /**
      * Gets the current temperature.
