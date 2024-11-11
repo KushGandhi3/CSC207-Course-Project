e5755a1db620579bd9ced15cb33fe8934d63cf7b
Daniil <sirdaniiltrukhin@gmail.com>
2024-11-10 21:58:08 -0500

    Completed DisplayHomeInputData DS

--- WiselyWearDemo/main/java/use_case/display_home/DisplayHomeInputData.java
+++ WiselyWearDemo/main/java/use_case/display_home/DisplayHomeInputData.java
@@ -1,23 +1,37 @@
 package use_case.display_home;
 
 /**
- * The Input Data for the Show-Current-Weather.java Use Case.
+ * The Input Data for the Display-Home Use Case.
  */
 public class DisplayHomeInputData {
 
+    public final int currentTemperature;
+    public final int highTemperature;
+    public final int lowTemperature;
+    public final String weatherCondition;
 
-    public DisplayHomeInputData() {
+    public DisplayHomeInputData(int currentTemperature, int highTemperature,
+                                int lowTemperature, String weatherCondition) {
+        this.currentTemperature = currentTemperature;
+        this.highTemperature = highTemperature;
+        this.lowTemperature = lowTemperature;
+        this.weatherCondition = weatherCondition;
     }
 
     public int getCurrentTemperature() {
+        return currentTemperature;
     }
 
     public int getHighTemperature() {
+        return highTemperature;
     }
 
     public int getLowTemperature() {
+        return lowTemperature;
     }
 
     public String getWeatherCondition() {
+        return weatherCondition;
     }
+
 }
