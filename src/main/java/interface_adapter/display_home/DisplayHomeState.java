3484f751e290f5b67eb654852ba6a5a61972c5c3
Kush Gandhi <99158764+KushGandhi3@users.noreply.github.com>
2024-11-17 21:36:45 -0500

    Implemented HomeView, DisplayHomeViewModel, and DisplayHomeState for the Home use case.

--- src/main/java/interface_adapter/display_home/DisplayHomeState.java
+++ src/main/java/interface_adapter/display_home/DisplayHomeState.java
@@ -1,4 +1,54 @@
 package interface_adapter.display_home;
 
+/**
+ * The state for the Home View Model
+ */
+
 public class DisplayHomeState {
+
+    private String city = "";
+    private String temperature = "";
+    private String highTemperature = "";
+    private String lowTemperature = "";
+    private String condition = "";
+
+    private String getCity() {
+        return this.city;
+    }
+
+    private String getTemperature() {
+        return this.temperature;
+    }
+
+    private String getHighTemperature() {
+        return this.highTemperature;
+    }
+
+    private String getLowTemperature() {
+        return this.lowTemperature;
+    }
+
+    private String getCondition() {
+        return this.condition;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+
+    private void setHighTemperature(String highTemperature) {
+        this.highTemperature = highTemperature;
+    }
+
+    private void setLowTemperature(String lowTemperature) {
+        this.lowTemperature = lowTemperature;
+    }
+
+    private void setCondition(String condition) {
+        this.condition = condition;
+    }
 }
--- src/main/java/interface_adapter/display_home/DisplayHomeViewModel.java
+++ src/main/java/interface_adapter/display_home/DisplayHomeViewModel.java
@@ -1,4 +1,14 @@
 package interface_adapter.display_home;
 
-public class DisplayHomeViewModel {
+import interface_adapter.ViewModel;
+
+/**
+ * The ViewModel for the HomeView
+ */
+public class DisplayHomeViewModel extends ViewModel<DisplayHomeState> {
+
+    public DisplayHomeViewModel() {
+        super("Home");
+        this.setState(new DisplayHomeState());
+    }
 }
