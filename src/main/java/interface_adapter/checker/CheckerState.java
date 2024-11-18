0df46f4d4382e9d132a4bdab86da08d5614e4f9e
chika <chika.peynisa@mail.utoronto.ca>
2024-11-17 20:00:11 -0500

    Updated CheckerState and its view model

--- src/main/java/interface_adapter/checker/CheckerState.java
+++ src/main/java/interface_adapter/checker/CheckerState.java
@@ -2,5 +2,51 @@ package interface_adapter.checker;
 
 public class CheckerState {
 
-    private String 
+    private String location = "Toronto";
+    private String tempOptions = "";
+    private int tempValue = 0;
+    private int startChecking = 0;
+    private int stopChecking = 1;
+
+    // Getters
+    public String getLocation() {
+        return location;
+    }
+
+    public String getTempOptions() {
+        return tempOptions;
+    }
+
+    public int getTempValue() {
+        return tempValue;
+    }
+
+    public int getStartChecking() {
+        return startChecking;
+    }
+
+    public int getStopChecking() {
+        return stopChecking;
+    }
+
+    // Setters
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setTempOptions(String tempOptions) {
+        this.tempOptions = tempOptions;
+    }
+
+    public void setTempValue(int tempValue) {
+        this.tempValue = tempValue;
+    }
+
+    public void setStartChecking(int startChecking) {
+        this.startChecking = startChecking;
+    }
+
+    public void setStopChecking(int stopChecking) {
+        this.stopChecking = stopChecking;
+    }
 }
--- src/main/java/interface_adapter/checker/CheckerViewModel.java
+++ src/main/java/interface_adapter/checker/CheckerViewModel.java
@@ -1,11 +1,23 @@
 package interface_adapter.checker;
 
 import  interface_adapter.ViewModel;
-import view.CheckerView;
 
 public class CheckerViewModel extends ViewModel<CheckerState> {
 
-    public static final String SAVE_BUTTON_LABEL = "Save";
+    public static final String TITLE_LABEL = "Weather Checker";
+    public static final String LOCATION_LABEL = "Location";
+    public static final String TEMPERATURE_LABEL = "Temperature";
+    public static final String TEMPERATURE_UNIT_LABEL = "°C";
+    public static final String START_CHECKING_LABEL = "Start Checking";
+    public static final String START_CHECKING_DESCRIPTION = "hour(s) from now";
+    public static final String STOP_CHECKING_LABEL = "Stop Checking";
+    public static final String STOP_CHECKING_DESCRIPTION = "hour(s) from start time";
+
+    public static final String[] TEMPERATURE_OPTIONS = new String[]{"Below", "At", "Above"};
+    public static final int[] START_CHECKING_OPTIONS = generateStartCheckingOptions();
+    public static final int[] STOP_CHECKING_OPTIONS = generateStopCheckingOptions();
+
+    public static final String CHECK_BUTTON_LABEL = "Check";
     public static final String CANCEL_BUTTON_LABEL = "Cancel";
 
     public CheckerViewModel() {
@@ -13,8 +25,25 @@ public class CheckerViewModel extends ViewModel<CheckerState> {
         setState(new CheckerState());
     }
 
-
-    public void addPropertyChangeListener(CheckerView checkerView) {
-
+    // Method to generate the start checking options list (0 to 48 hours)
+    private static int[] generateStartCheckingOptions() {
+        int[] options = new int[49]; // Array of size 49 (0 to 48)
+        for (int i = 0; i <= 48; i++) {
+            options[i] = i;
+        }
+        return options;
     }
+
+    // Method to generate the stop checking options list (1 to 24 hours)
+    private static int[] generateStopCheckingOptions() {
+        int[] options = new int[24]; // Array of size 24 (1 to 24)
+        for (int i = 0; i < 24; i++) {
+            options[i] = (i+1);
+        }
+        return options;
+    }
+
+
+
+
 }
