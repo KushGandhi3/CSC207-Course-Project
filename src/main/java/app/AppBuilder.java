a6694b9cc5939caa71fed52ed1f53a700fecf730
Kush Gandhi <99158764+KushGandhi3@users.noreply.github.com>
2024-11-17 21:37:22 -0500

    Implemented basic functionality for testing purposes within AppBuilder and Main.

--- src/main/java/app/AppBuilder.java
+++ src/main/java/app/AppBuilder.java
@@ -24,60 +24,60 @@ import interface_adapter.display_home.DisplayHomeState;
 import interface_adapter.display_home.DisplayHomeViewModel;
 import view.HomeView;
 
-// Daily Page
-import use_case.display_daily.DisplayDailyDataAccessInterface;
-import use_case.display_daily.DisplayDailyInteractor;
-import use_case.display_daily.DisplayDailyInputBoundary;
-import use_case.display_daily.DisplayDailyOutputBoundary;
-import interface_adapter.display_daily.DisplayDailyController;
-import interface_adapter.display_daily.DisplayDailyPresenter;
-import interface_adapter.display_daily.DisplayDailyState;
-import interface_adapter.display_daily.DisplayDailyViewModel;
-import view.DailyView;
-
-// Weekly Page
-import use_case.display_weekly.DisplayWeeklyDataAccessInterface;
-import use_case.display_weekly.DisplayWeeklyInteractor;
-import use_case.display_weekly.DisplayWeeklyInputBoundary;
-import use_case.display_weekly.DisplayWeeklyOutputBoundary;
-import interface_adapter.display_weekly.DisplayWeeklyController;
-import interface_adapter.display_weekly.DisplayWeeklyPresenter;
-import interface_adapter.display_weekly.DisplayWeeklyState;
-import interface_adapter.display_weekly.DisplayWeeklyViewModel;
-import view.WeeklyView;
-
-// Alerts Page
-import use_case.set_alerts.SetAlertsDataAccessInterface;
-import use_case.set_alerts.SetAlertsInteractor;
-import use_case.set_alerts.SetAlertsInputBoundary;
-import use_case.set_alerts.SetAlertsOutputBoundary;
-import interface_adapter.set_alerts.SetAlertController;
-import interface_adapter.set_alerts.SetAlertPresenter;
-import interface_adapter.set_alerts.SetAlertState;
-import interface_adapter.set_alerts.SetAlertViewModel;
-import view.SetAlertsView;
-
-// Outfit Page
-import use_case.analyze_outfit.AnalyzeOutfitDataAccessInterface;
-import use_case.analyze_outfit.AnalyzeOutfitInteractor;
-import use_case.analyze_outfit.AnalyzeOutfitInputBoundary;
-import use_case.analyze_outfit.AnalyzeOutfitOutputBoundary;
-import interface_adapter.analyze_outfit.AnalyzeOutfitController;
-import interface_adapter.analyze_outfit.AnalyzeOutfitPresenter;
-import interface_adapter.analyze_outfit.AnalyzeOutfitState;
-import interface_adapter.analyze_outfit.AnaylzeOutfitViewModel;
-import view.AnalyzeOutfitView;
-
-// Map Page
-import use_case.display_map.DisplayMapDataAccessInterface;
-import use_case.display_map.DisplayMapInteractor;
-import use_case.display_map.DisplayMapInputBoundary;
-import use_case.display_map.DisplayMapOutputBoundary;
-import interface_adapter.display_map.DisplayMapController;
-import interface_adapter.display_map.DisplayMapPresenter;
-import interface_adapter.display_map.DisplayMapState;
-import interface_adapter.display_map.DisplayMapViewModel;
-import view.MapView;
+//// Daily Page
+//import use_case.display_daily.DisplayDailyDataAccessInterface;
+//import use_case.display_daily.DisplayDailyInteractor;
+//import use_case.display_daily.DisplayDailyInputBoundary;
+//import use_case.display_daily.DisplayDailyOutputBoundary;
+//import interface_adapter.display_daily.DisplayDailyController;
+//import interface_adapter.display_daily.DisplayDailyPresenter;
+//import interface_adapter.display_daily.DisplayDailyState;
+//import interface_adapter.display_daily.DisplayDailyViewModel;
+//import view.DailyView;
+//
+//// Weekly Page
+//import use_case.display_weekly.DisplayWeeklyDataAccessInterface;
+//import use_case.display_weekly.DisplayWeeklyInteractor;
+//import use_case.display_weekly.DisplayWeeklyInputBoundary;
+//import use_case.display_weekly.DisplayWeeklyOutputBoundary;
+//import interface_adapter.display_weekly.DisplayWeeklyController;
+//import interface_adapter.display_weekly.DisplayWeeklyPresenter;
+//import interface_adapter.display_weekly.DisplayWeeklyState;
+//import interface_adapter.display_weekly.DisplayWeeklyViewModel;
+//import view.WeeklyView;
+//
+//// Alerts Page
+//import use_case.set_alerts.SetAlertsDataAccessInterface;
+//import use_case.set_alerts.SetAlertsInteractor;
+//import use_case.set_alerts.SetAlertsInputBoundary;
+//import use_case.set_alerts.SetAlertsOutputBoundary;
+//import interface_adapter.set_alerts.SetAlertController;
+//import interface_adapter.set_alerts.SetAlertPresenter;
+//import interface_adapter.set_alerts.SetAlertState;
+//import interface_adapter.set_alerts.SetAlertViewModel;
+//import view.SetAlertsView;
+//
+//// Outfit Page
+//import use_case.analyze_outfit.AnalyzeOutfitDataAccessInterface;
+//import use_case.analyze_outfit.AnalyzeOutfitInteractor;
+//import use_case.analyze_outfit.AnalyzeOutfitInputBoundary;
+//import use_case.analyze_outfit.AnalyzeOutfitOutputBoundary;
+//import interface_adapter.analyze_outfit.AnalyzeOutfitController;
+//import interface_adapter.analyze_outfit.AnalyzeOutfitPresenter;
+//import interface_adapter.analyze_outfit.AnalyzeOutfitState;
+//import interface_adapter.analyze_outfit.AnaylzeOutfitViewModel;
+//import view.AnalyzeOutfitView;
+//
+//// Map Page
+//import use_case.display_map.DisplayMapDataAccessInterface;
+//import use_case.display_map.DisplayMapInteractor;
+//import use_case.display_map.DisplayMapInputBoundary;
+//import use_case.display_map.DisplayMapOutputBoundary;
+//import interface_adapter.display_map.DisplayMapController;
+//import interface_adapter.display_map.DisplayMapPresenter;
+//import interface_adapter.display_map.DisplayMapState;
+//import interface_adapter.display_map.DisplayMapViewModel;
+//import view.MapView;
 
 /**
  * The AppBuilder class is responsible for building the application.
