74af4857ab1a6d128fb7cc1091a37ab0a97a70ee
jannate-r <jannate.rahim1@gmail.com>
2024-11-21 14:56:44 -0500

    Fixed errors in the Controller and Presenter

--- src/main/java/interface_adapter/display_home/DisplayHomeController.java
+++ src/main/java/interface_adapter/display_home/DisplayHomeController.java
@@ -1,21 +1,36 @@
 package interface_adapter.display_home;
-import use_case.checker.CheckerInputBoundary;
+
 import use_case.display_home.DisplayHomeInputBoundary;
 import use_case.display_home.DisplayHomeInputData;
 
+/**
+ * The controller for the Display Home Use Case.
+ */
 public class DisplayHomeController {
-    /**
-     * Controller for DisplayHome related Use Case.
-     */
-    private final DisplayHomeInputBoundary displayHomeInteractor;
-    public DisplayHomeController(DisplayHomeInputBoundary displayHomeInteractor) {
-        this.displayHomeInteractor = displayHomeInteractor;
+
+    private final DisplayHomeInputBoundary displayHomeUseCaseInteractor;
+
+    public DisplayHomeController(DisplayHomeInputBoundary displayHomeUseCaseInteractor) {
+        this.displayHomeUseCaseInteractor = displayHomeUseCaseInteractor;
     }
+
     /**
-     * Executes the DisplayHome Use Case.
+     * Executes the Display Home Use Case.
+     * @param cityName the name of the city to fetch weather data for
+     * @param daily whether the user wants the daily weather
+     * @param weekly whether the user wants the weekly weather
+     * @param checker whether the user wants a weather checker
+     * @param outfit whether the user wants outfit suggestions
+     * @param map whether the user wants to see a map
      */
-    public void execute() {
-        //ToDo
-        displayHomeInteractor.execute(DisplayHomeInputData);
+    public void execute(String cityName, boolean daily, boolean weekly, boolean checker, boolean outfit, boolean map) {
+        // Create the input data for the use case
+        final DisplayHomeInputData displayHomeInputData = new DisplayHomeInputData(
+                cityName, daily, weekly, checker, outfit, map
+        );
+
+        // Execute the Interactor
+        displayHomeUseCaseInteractor.execute(displayHomeInputData);
     }
+
 }
--- src/main/java/interface_adapter/display_home/DisplayHomePresenter.java
+++ src/main/java/interface_adapter/display_home/DisplayHomePresenter.java
@@ -1,52 +1,52 @@
 package interface_adapter.display_home;
 
 import interface_adapter.ViewManagerModel;
-import use_case.display_home.DisplayHomeInputBoundary;
+import use_case.display_home.DisplayHomeOutputBoundary;
 import use_case.display_home.DisplayHomeOutputData;
 
 /**
- * The Presenter for the DisplayHome Use Case.
+ * The Presenter for the Display Home Use Case.
  */
-public class DisplayHomePresenter extends DisplayHomeInputBoundary {
+public class DisplayHomePresenter implements DisplayHomeOutputBoundary {
+
     private final DisplayHomeViewModel displayHomeViewModel;
     private final ViewManagerModel viewManagerModel;
 
-    public DisplayHomePresenter(ViewManagerModel viewManagerModel, DisplayHomeViewModel displayHomeViewModel) {
-        this.displayHomeViewModel = displayHomeViewModel;
+    public DisplayHomePresenter(ViewManagerModel viewManagerModel,
+                                DisplayHomeViewModel displayHomeViewModel) {
         this.viewManagerModel = viewManagerModel;
-    }
-
-    public static void handleDailyWeatherRequest() {
-        //TODO
-    }
-
-    public static void handleWeeklyWeatherRequest() {
-        //TODO
-    }
-
-    public static void handleWeatherCheckerRequest() {
-        //TODO
-    }
-
-    public static void handleOutfitSuggestionsRequest() {
-        //TODO
-    }
-
-    public static void handleMapRequest() {
-        //TODO
+        this.displayHomeViewModel = displayHomeViewModel;
     }
 
     @Override
     public void prepareSuccessView(DisplayHomeOutputData response) {
-        // On success, switch to the DisplayHome in view.
+        // On success, update the state with weather data.
 
-        final DisplayHomeState loggedInState = displayHomeViewModel.getState();
-        this.viewManagerModel.setState(displayHomeViewModel.getViewName());
+        DisplayHomeState state = displayHomeViewModel.getState();
+
+        // Update the weather data state
+        state.setCity(response.getWeatherData().getCity());
+        state.setTemperature(response.getWeatherData().getTemperature());
+        state.setHighTemperature(response.getWeatherData().getHighTemperature());
+        state.setLowTemperature(response.getWeatherData().getLowTemperature());
+        state.setCondition(response.getWeatherData().getCondition());
+
+        // Notify the view model that data has been updated
+        displayHomeViewModel.firePropertyChanged();
+
+        // Update the view manager state to "DisplayHome"
+        this.viewManagerModel.setState("DisplayHome");
         this.viewManagerModel.firePropertyChanged();
     }
 
     @Override
     public void prepareFailView(String error) {
+        // In case of an error, set the error state
+        DisplayHomeState state = displayHomeViewModel.getState();
+        state.setCondition(error); // Using the condition to store the error message
+
+        // Notify the view model that data has been updated
         displayHomeViewModel.firePropertyChanged();
     }
+    
 }
