7c6e513beade6698562a4c725fb0deaeed462b3d
jannate-r <jannate.rahim1@gmail.com>
2024-11-20 22:25:52 -0500

    Implemented the DisplayHomeController and DisplayHomePresenter.

--- src/main/java/interface_adapter/display_home/DisplayHomeController.java
+++ src/main/java/interface_adapter/display_home/DisplayHomeController.java
@@ -1,4 +1,21 @@
 package interface_adapter.display_home;
+import use_case.checker.CheckerInputBoundary;
+import use_case.display_home.DisplayHomeInputBoundary;
+import use_case.display_home.DisplayHomeInputData;
 
 public class DisplayHomeController {
+    /**
+     * Controller for DisplayHome related Use Case.
+     */
+    private final DisplayHomeInputBoundary displayHomeInteractor;
+    public DisplayHomeController(DisplayHomeInputBoundary displayHomeInteractor) {
+        this.displayHomeInteractor = displayHomeInteractor;
+    }
+    /**
+     * Executes the DisplayHome Use Case.
+     */
+    public void execute() {
+        //ToDo
+        displayHomeInteractor.execute(DisplayHomeInputData);
+    }
 }
--- src/main/java/interface_adapter/display_home/DisplayHomePresenter.java
+++ src/main/java/interface_adapter/display_home/DisplayHomePresenter.java
@@ -1,4 +1,52 @@
 package interface_adapter.display_home;
 
-public class DisplayHomePresenter {
+import interface_adapter.ViewManagerModel;
+import use_case.display_home.DisplayHomeInputBoundary;
+import use_case.display_home.DisplayHomeOutputData;
+
+/**
+ * The Presenter for the DisplayHome Use Case.
+ */
+public class DisplayHomePresenter extends DisplayHomeInputBoundary {
+    private final DisplayHomeViewModel displayHomeViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public DisplayHomePresenter(ViewManagerModel viewManagerModel, DisplayHomeViewModel displayHomeViewModel) {
+        this.displayHomeViewModel = displayHomeViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    public static void handleDailyWeatherRequest() {
+        //TODO
+    }
+
+    public static void handleWeeklyWeatherRequest() {
+        //TODO
+    }
+
+    public static void handleWeatherCheckerRequest() {
+        //TODO
+    }
+
+    public static void handleOutfitSuggestionsRequest() {
+        //TODO
+    }
+
+    public static void handleMapRequest() {
+        //TODO
+    }
+
+    @Override
+    public void prepareSuccessView(DisplayHomeOutputData response) {
+        // On success, switch to the DisplayHome in view.
+
+        final DisplayHomeState loggedInState = displayHomeViewModel.getState();
+        this.viewManagerModel.setState(displayHomeViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        displayHomeViewModel.firePropertyChanged();
+    }
 }
