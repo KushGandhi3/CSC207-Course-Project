74ad61cf1016e365689d06ac081d25b1a01cf864
jannate-r <jannate.rahim1@gmail.com>
2024-11-21 13:54:03 -0500

    Implemented the DisplayHomeInputBoundary AND DisplayHomeInputData.

--- src/main/java/use_case/display_home/DisplayHomeInputBoundary.java
+++ src/main/java/use_case/display_home/DisplayHomeInputBoundary.java
@@ -1,4 +1,12 @@
 package use_case.display_home;
 
-public class DisplayHomeInputBoundary {
+/**
+ * Input Boundary for actions which are related to the DisplayHome.
+ */
+public interface DisplayHomeInputBoundary {
+    /**
+     * Executes the DisplayHome use case.
+     * @param displayHomeInputData the input data
+     */
+    void execute(DisplayHomeInputData displayHomeInputData);
 }
--- src/main/java/use_case/display_home/DisplayHomeInputData.java
+++ src/main/java/use_case/display_home/DisplayHomeInputData.java
@@ -1,4 +1,58 @@
 package use_case.display_home;
 
+/**
+ * The Input Data for the DisplayHome Use Case.
+ */
+
 public class DisplayHomeInputData {
+
+    private final String cityName;
+    private final boolean dailyButtonClicked;
+    private final boolean weeklyButtonClicked;
+    private final boolean checkerButtonClicked;
+    private final boolean outfitButtonClicked;
+    private final boolean mapButtonClicked;
+
+    /**
+     * Constructor to initialize DisplayHomeInputData with all necessary fields.
+     * @param cityName The city name for the weather display.
+     * @param dailyButtonClicked Whether the "Daily" button was clicked.
+     * @param weeklyButtonClicked Whether the "Weekly" button was clicked.
+     * @param checkerButtonClicked Whether the "Checker" button was clicked.
+     * @param outfitButtonClicked Whether the "Outfit" button was clicked.
+     * @param mapButtonClicked Whether the "Map" button was clicked.
+     */
+    public DisplayHomeInputData(String cityName, boolean dailyButtonClicked, boolean weeklyButtonClicked,
+                                boolean checkerButtonClicked, boolean outfitButtonClicked, boolean mapButtonClicked) {
+        this.cityName = cityName;
+        this.dailyButtonClicked = dailyButtonClicked;
+        this.weeklyButtonClicked = weeklyButtonClicked;
+        this.checkerButtonClicked = checkerButtonClicked;
+        this.outfitButtonClicked = outfitButtonClicked;
+        this.mapButtonClicked = mapButtonClicked;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public boolean isDailyButtonClicked() {
+        return dailyButtonClicked;
+    }
+
+    public boolean isWeeklyButtonClicked() {
+        return weeklyButtonClicked;
+    }
+
+    public boolean isCheckerButtonClicked() {
+        return checkerButtonClicked;
+    }
+
+    public boolean isOutfitButtonClicked() {
+        return outfitButtonClicked;
+    }
+
+    public boolean isMapButtonClicked() {
+        return mapButtonClicked;
+    }
 }
