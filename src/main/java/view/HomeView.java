69221af5b3eb63a8d591f71fba4027e34e1ad6cc
jannate-r <jannate.rahim1@gmail.com>
2024-11-20 22:24:43 -0500

    Changed the bounds of the components. Added Listeners and Actions for the buttons.

--- src/main/java/view/HomeView.java
+++ src/main/java/view/HomeView.java
@@ -1,5 +1,6 @@
 package view;
 
+import interface_adapter.display_home.DisplayHomePresenter;
 import interface_adapter.display_home.DisplayHomeViewModel;
 
 import java.awt.Component;
@@ -16,19 +17,27 @@ import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
-public class HomeView extends JPanel {
+import interface_adapter.display_home.DisplayHomeController;
+import interface_adapter.display_home.DisplayHomeState;
+import interface_adapter.display_home.DisplayHomeViewModel;
+
+/**
+ * The View for the Display Home Use Case.
+ */
+
+public class HomeView extends JPanel implements ActionListener,PropertyChangeListener {
 
     // View Model
-    private DisplayHomeViewModel homeViewModel;
+    private final DisplayHomeViewModel homeViewModel;
 
     // View Name
     public final String viewName = "Home";
 
     // Weather Data Labels
-    private JLabel temperatureLabel;
-    private JLabel highTemperatureLabel;
-    private JLabel lowTemperatureLabel;
-    private JLabel conditionLabel;
+    private final JLabel temperatureLabel;
+    private final JLabel highTemperatureLabel;
+    private final JLabel lowTemperatureLabel;
+    private final JLabel conditionLabel;
 
     // City Label
     private JLabel cityLabel; // TODO: This needs to be a input field later on
@@ -37,14 +46,15 @@ public class HomeView extends JPanel {
     private JLabel timeLabel;
 
     // Buttons
-    private JButton DailyButton;
-    private JButton WeeklyButton;
-    private JButton CheckerButton;
-    private JButton OutfitButton;
-    private JButton MapButton;
+    private final JButton DailyButton;
+    private final JButton WeeklyButton;
+    private final JButton CheckerButton;
+    private final JButton OutfitButton;
+    private final JButton MapButton;
 
     public HomeView(DisplayHomeViewModel homeViewModel) {
         this.homeViewModel = homeViewModel;
+        this.homeViewModel.addPropertyChangeListener(this);
 
         // Set the layout
         this.setLayout(null); // Absolute positioning
@@ -83,11 +93,111 @@ public class HomeView extends JPanel {
         this.add(MapButton);
 
         // Set the bounds of the components
-        cityLabel.setBounds(10, 10, 100, 20);
-        temperatureLabel.setBounds(10, 30, 100, 20);
-        highTemperatureLabel.setBounds(10, 50, 100, 20);
-        lowTemperatureLabel.setBounds(10, 70, 100, 20);
-        conditionLabel.setBounds(10, 90, 100, 20);
-        timeLabel.setBounds(10, 110, 100, 20);
+        cityLabel.setBounds(461, 216, 278, 68);
+        temperatureLabel.setBounds(502, 316, 196, 125);
+        highTemperatureLabel.setBounds(567, 430, 87, 42);
+        lowTemperatureLabel.setBounds(670, 430, 80, 42);
+        conditionLabel.setBounds(476, 430, 79, 42);
+        timeLabel.setBounds(544, 288, 112, 42);
+    }
+    // Setup listeners for button clicks
+
+    private void setupListeners() {
+        // Listener for the 'Daily' button
+        DailyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onDailyButtonClicked();
+            }
+        });
+
+        // Listener for the 'Weekly' button
+        WeeklyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onWeeklyButtonClicked();
+            }
+        });
+
+        // Listener for the 'Checker' button
+        CheckerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onCheckerButtonClicked();
+            }
+        });
+
+        // Listener for the 'Outfit' button
+        OutfitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onOutfitButtonClicked();
+            }
+        });
+
+        // Listener for the 'Map' button
+        MapButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                onMapButtonClicked();
+            }
+        });
+    }
+
+    // Action when "Daily" button is clicked
+    private void onDailyButtonClicked() {
+        DisplayHomePresenter.handleDailyWeatherRequest();
+    }
+
+    // Action when "Weekly" button is clicked
+    private void onWeeklyButtonClicked() {
+        DisplayHomePresenter.handleWeeklyWeatherRequest();
+    }
+
+    // Action when "Checker" button is clicked
+    private void onCheckerButtonClicked() {
+        DisplayHomePresenter.handleWeatherCheckerRequest();
+    }
+
+    // Action when "Outfit" button is clicked
+    private void onOutfitButtonClicked() {
+        DisplayHomePresenter.handleOutfitSuggestionsRequest();
+    }
+
+    // Action when "Map" button is clicked
+    private void onMapButtonClicked() {
+        DisplayHomePresenter.handleMapRequest();
+    }
+
+    // Method to update the temperature label
+    public void setTemperatureLabel(String text) {
+        temperatureLabel.setText(text);
+    }
+
+    // Method to update the high temperature label
+    public void setHighTemperatureLabel(String text) {
+        highTemperatureLabel.setText(text);
+    }
+
+    // Method to update the low temperature label
+    public void setLowTemperatureLabel(String text) {
+        lowTemperatureLabel.setText(text);
+    }
+
+    // Method to update the condition label
+    public void setConditionLabel(String text) {
+        conditionLabel.setText(text);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        // TODO: Implement property change events
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //TODO
     }
 }
+
