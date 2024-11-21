f703e0344a5d013bea777896bf0d2957b5147952
jannate-r <jannate.rahim1@gmail.com>
2024-11-21 14:32:33 -0500

    Implemented the UseCase for DisplayHome

--- src/main/java/use_case/display_home/DisplayHomeOutputBoundary.java
+++ src/main/java/use_case/display_home/DisplayHomeOutputBoundary.java
@@ -1,4 +1,19 @@
 package use_case.display_home;
 
-public class DisplayHomeOutputBoundary {
+/**
+ * Teh output boundary for the DisplayHome Use Case.
+ */
+public interface DisplayHomeOutputBoundary {
+    /**
+     * Prepares the success view for the DisplayHome Use Case
+     * @param outputData the output data
+     */
+    void prepareSuccessView(DisplayHomeOutputData outputData);
+
+
+    /**
+     * Prepares the failure view for the DisplayHome Use Case.
+     * @param errorMessage the explanation of the failure.
+     */
+    void prepareFailView(String errorMessage);
 }
