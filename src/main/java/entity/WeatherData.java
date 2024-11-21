6d53f9bb3f42663032e782570487eb92d23be2d6
jannate-r <jannate.rahim1@gmail.com>
2024-11-21 14:10:05 -0500

    Implemented the WeatherData

--- src/main/java/entity/WeatherData.java
+++ src/main/java/entity/WeatherData.java
@@ -1,5 +1,32 @@
 package entity;
 
-// TODO: Implement the WeatherData class [TEAM]
 public class WeatherData {
+
+    private final String temperature;
+    private final String highTemperature;
+    private final String lowTemperature;
+    private final String condition;
+
+    public WeatherData(String temperature, String highTemperature, String lowTemperature, String condition) {
+        this.temperature = temperature;
+        this.highTemperature = highTemperature;
+        this.lowTemperature = lowTemperature;
+        this.condition = condition;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public String getHighTemperature() {
+        return highTemperature;
+    }
+
+    public String getLowTemperature() {
+        return lowTemperature;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
 }
