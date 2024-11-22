fb5794f6c03b63a427738fcf0ce62186373b7347
chika <chika.peynisa@mail.utoronto.ca>
2024-11-21 20:35:39 -0500

    Implemented CheckerInputBoundary and CheckerOutputBoundary

--- src/main/java/use_case/checker/CheckerInputBoundary.java
+++ src/main/java/use_case/checker/CheckerInputBoundary.java
@@ -1,4 +1,15 @@
 package use_case.checker;
 
 public interface CheckerInputBoundary {
+
+    /**
+     * Execute the checker use case
+     * @param checkerInputData the input data
+     */
+    void execute(CheckerInputData checkerInputData);
+
+    /**
+     * Executes the switch to the home view use case
+     */
+    void switchToHomeView();
 }
--- src/main/java/use_case/checker/CheckerOutputBoundary.java
+++ src/main/java/use_case/checker/CheckerOutputBoundary.java
@@ -1,4 +1,20 @@
 package use_case.checker;
 
 public interface CheckerOutputBoundary {
+    /**
+     * Prepare the success view with the given exist string.
+     * @param exist the string to display in the success view
+     */
+    void prepareCondMetView(String exist);
+
+    /**
+     * Prepare the fail view with the given nonexist string.
+     * @param nonexist the string to display in the fail view
+     */
+    void prepareCondNotMetView(String nonexist);
+
+    /**
+     * Switch to the home view.
+     */
+    void switchToHomeView();
 }
