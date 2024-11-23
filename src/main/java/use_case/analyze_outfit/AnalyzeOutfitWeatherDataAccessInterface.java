32a4b4fc4c0da30a686487725277f25bf068f3ee
Kush Gandhi <99158764+KushGandhi3@users.noreply.github.com>
2024-11-22 20:59:17 -0500

    Updated all Use Case DA Interfaces corresponding to the new WeatherDataDAO.

--- src/main/java/use_case/analyze_outfit/AnalyzeOutfitWeatherDataAccessInterface.java
+++ src/main/java/use_case/analyze_outfit/AnalyzeOutfitWeatherDataAccessInterface.java
@@ -1,4 +1,4 @@
-package use_case.display_daily;
+package use_case.analyze_outfit;
 
 import entity.weather_data.WeatherData;
 import exception.APICallException;
@@ -6,7 +6,7 @@ import exception.APICallException;
 /**
  * The interface of the DAO for the weather data used by all use cases.
  */
-public interface DisplayDailyDataAccessInterface {
+public interface AnalyzeOutfitWeatherDataAccessInterface {
 
     /**
      * Get Weather data from the API.
--- src/main/java/use_case/checker/CheckerDataAccessInterface.java
+++ src/main/java/use_case/checker/CheckerDataAccessInterface.java
@@ -1,4 +1,20 @@
 package use_case.checker;
 
+import java.io.IOException;
+
+import entity.weather_data.WeatherData;
+
+/**
+ * The interface of the DAO for the weather data used by all use cases.
+ */
 public interface CheckerDataAccessInterface {
+
+    /**
+     * Get Weather data from the API.
+     * @param location the name of the location.
+     * @return the weather data.
+     * @throws IOException if the request fails.
+     */
+    WeatherData getWeatherData(String location) throws IOException;
 }
+
