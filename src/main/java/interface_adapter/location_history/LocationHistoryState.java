2c94c46826491d19b6f32a1a711593ab642daa9e
chika <chika.peynisa@mail.utoronto.ca>
2024-11-22 22:03:16 -0500

    Added the packages and files for location_history

---
+++ src/main/java/interface_adapter/location_history/LocationHistoryState.java
@@ -0,0 +1,4 @@
+package interface_adapter.location_history;
+
+public class LocationHistoryState {
+}
---
+++ src/main/java/use_case/location_history/LocationHistoryInteractor.java
@@ -0,0 +1,4 @@
+package use_case.location_history;
+
+public class LocationHistoryInteractor {
+}
