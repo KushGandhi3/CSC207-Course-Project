454db2b49794354da874d6d52a25218afead1a41
Diana Akhmedova <katnissgamer001@gmail.com>
2024-11-25 05:15:56 -0500

    initial use case update for hourly

--- src/main/java/interface_adapter/display_hourly/DisplayHourlyController.java
+++ src/main/java/interface_adapter/display_hourly/DisplayHourlyController.java
@@ -1,4 +1,5 @@
 package interface_adapter.display_hourly;
 
 public class DisplayHourlyController {
+    // TODO: Complete class
 }
--- src/main/java/use_case/display_hourly/DisplayHourlyInputData.java
+++ src/main/java/use_case/display_hourly/DisplayHourlyInputData.java
@@ -1,4 +1,13 @@
 package use_case.display_hourly;
 
 public class DisplayHourlyInputData {
+    private final String city;
+
+    public DisplayHourlyInputData(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
 }
