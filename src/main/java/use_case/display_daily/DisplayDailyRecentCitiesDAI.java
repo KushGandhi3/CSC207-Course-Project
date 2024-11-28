f1ce37593416b980f0f1a4d7f3acb78a3bda36f5
Daniil <sirdaniiltrukhin@gmail.com>
2024-11-27 20:41:14 -0500

    made getRecentCityData() specify an exception

--- src/main/java/use_case/display_daily/DisplayDailyRecentCitiesDAI.java
+++ src/main/java/use_case/display_daily/DisplayDailyRecentCitiesDAI.java
@@ -1,9 +1,10 @@
 package use_case.display_daily;
 
 import entity.recent_city.RecentCityData;
+import exception.RecentCitiesDataException;
 
 public interface DisplayDailyRecentCitiesDAI {
 
-    RecentCityData getRecentCityData();
+    RecentCityData getRecentCityData() throws RecentCitiesDataException;
 
 }
