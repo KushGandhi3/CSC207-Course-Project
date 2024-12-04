20844469b124a95d8e0bb0ce3a37775aa5c3cf95
Kush Gandhi <99158764+KushGandhi3@users.noreply.github.com>
2024-12-04 18:51:55 -0500

    Fixed Checkstyle errors in display_checker interface adapters.

--- src/main/java/interface_adapter/display_checker/DisplayCheckerState.java
+++ src/main/java/interface_adapter/display_checker/DisplayCheckerState.java
@@ -6,19 +6,47 @@ public class DisplayCheckerState {
     private String weatherConditionOptions = "";
     private int startChecking;
     private int stopChecking;
-    private String message = null;
+    private String message;
 
     // Getters
-    public String getLocation() {return location;}
-    public String getWeatherConditionOptions() {return weatherConditionOptions;}
-    public int getStartChecking() {return startChecking;}
-    public int getStopChecking() {return stopChecking;}
-    public String getMessage() { return message; }
+    public String getLocation() {
+        return location;
+    }
+
+    public String getWeatherConditionOptions() {
+        return weatherConditionOptions;
+    }
+
+    public int getStartChecking() {
+        return startChecking;
+    }
+
+    public int getStopChecking() {
+        return stopChecking;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 
     // Setters
-    public void setLocation(String location) {this.location = location;}
-    public void setWeatherConditionOptions(String weatherConditionOptions) {this.weatherConditionOptions = weatherConditionOptions;}
-    public void setStartChecking(int startChecking) {this.startChecking = startChecking;}
-    public void setStopChecking(int stopChecking) {this.stopChecking = stopChecking;}
-    public void setMessage(String nonexist) { this.message = nonexist; }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setWeatherConditionOptions(String weatherConditionOptions) {
+        this.weatherConditionOptions = weatherConditionOptions;
+    }
+
+    public void setStartChecking(int startChecking) {
+        this.startChecking = startChecking;
+    }
+
+    public void setStopChecking(int stopChecking) {
+        this.stopChecking = stopChecking;
+    }
+
+    public void setMessage(String nonexist) {
+        this.message = nonexist;
+    }
 }
--- src/main/java/interface_adapter/display_checker/DisplayCheckerViewModel.java
+++ src/main/java/interface_adapter/display_checker/DisplayCheckerViewModel.java
@@ -1,9 +1,10 @@
 package interface_adapter.display_checker;
 
-import interface_adapter.ViewModel;
-
-import javax.swing.*;
 import java.util.Objects;
+
+import javax.swing.ImageIcon;
+
+import interface_adapter.ViewModel;
 
 public class DisplayCheckerViewModel extends ViewModel<DisplayCheckerState> {
 
@@ -14,10 +15,12 @@ public class DisplayCheckerViewModel extends ViewModel<DisplayCheckerState> {
     public static final String START_CHECKING_DESCRIPTION = "hour(s) from now";
     public static final String STOP_CHECKING_LABEL = "Stop Checking";
     public static final String STOP_CHECKING_DESCRIPTION = "hour(s) from start time";
-    public static final String[] WEATHER_CONDITION_OPTIONS = new String[]{"Thunderstorm", "Drizzle", "Rain", "Snow", "Clear",
-            "Clouds"};
+    public static final String[] WEATHER_CONDITION_OPTIONS = new String[]{"Thunderstorm", "Drizzle", "Rain",
+                                                                          "Snow", "Clear", "Clouds"};
     public static final Integer[] START_CHECKING_OPTIONS = generateStartCheckingOptions();
     public static final Integer[] STOP_CHECKING_OPTIONS = generateStopCheckingOptions();
+    public static final int NUM_24 = 24;
+    public static final int NUM_25 = 25;
 
     public static final String CHECK_BUTTON_LABEL = "Check";
     public static final ImageIcon BACK_BUTTON_IMAGE = new ImageIcon(
@@ -31,8 +34,8 @@ public class DisplayCheckerViewModel extends ViewModel<DisplayCheckerState> {
 
     // Method to generate the start checking options list (0 to 24 hours)
     private static Integer[] generateStartCheckingOptions() {
-        Integer[] options = new Integer[25]; // Array of size 25 (0 to 24)
-        for (int i = 0; i <= 24; i++) {
+        final Integer[] options = new Integer[NUM_25];
+        for (int i = 0; i <= NUM_24; i++) {
             options[i] = i;
         }
         return options;
@@ -40,9 +43,9 @@ public class DisplayCheckerViewModel extends ViewModel<DisplayCheckerState> {
 
     // Method to generate the stop checking options list (1 to 24 hours)
     private static Integer[] generateStopCheckingOptions() {
-        Integer[] options = new Integer[24]; // Array of size 24 (1 to 24)
-        for (int i = 0; i < 24; i++) {
-            options[i] = (i+1);
+        final Integer[] options = new Integer[NUM_24];
+        for (int i = 0; i < NUM_24; i++) {
+            options[i] = i + 1;
         }
         return options;
     }
